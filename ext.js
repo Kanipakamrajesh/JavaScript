df3e0440e68abc7fccb63063e997e71e122ae5e8
Kanipakamrajesh <rajeshraju5015@gmail.com>
2024-12-26 13:18:32 +0530

    Java Script

---
+++ ext.js
@@ -0,0 +1,9 @@
+console.log("This is External File");
+console.error("This is External File");
+console.warn("This is External File");
+function main()
+{
+	console.log("main Starts");
+	console.log("main ends");
+}
+main();
